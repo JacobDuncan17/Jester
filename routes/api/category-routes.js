f1466a805717ae2499ab3d18e7764b3684ce089b
Jacob Duncan <duncan@Jacobs-MacBook-Pro.local>
2023-04-16 12:01:01 -0600

    category-routes completed

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,28 +1,60 @@
 const router = require('express').Router();
+const { restart } = require('nodemon');
 const { Category, Product } = require('../../models');
 
-// The `/api/categories` endpoint
-
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+router.get('/', async(req, res) => {
+    const allCategories = await Category.findAll({
+      include: [{ model: Product}],
+    });
+    res.status(200).json(allCategories);
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get('/:id', async(req, res) => {
+    const categoryId = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+    if (!categoryId) {
+      res.status(404).json({ message: 'There is not a category found with this id'});
+      return;
+    }
+    res.status(200).json(categoryId);
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+router.post('/', async (req, res) => {
+    const newCategory = await Category.create(req.body);
+    res.status(201).json(newCategory);
 });
 
 router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+  Category.update(req.body, { where: { id: req.params.id } })
+    .then(() => {
+      return Category.findOne({
+        where: { id: req.params.id },
+        include: [{ model: Product }],
+      });
+    })
+    .then((category) => {
+      res.status(200).json(category);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
 });
 
 router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(() =>{
+    res.status(200).json({ message: 'Category has been deleted'})
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(400).json(err);
+  })
 });
 
 module.exports = router;
